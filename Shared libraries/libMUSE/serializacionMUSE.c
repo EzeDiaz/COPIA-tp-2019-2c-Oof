4685f7dddfdd4faa265d7cf05b8a63b7be2cf500
EzeDiaz <ezequiel.dz@gmail.com>
2019-09-23 16:50:12 -0300

    Arranco funciones de serializacion

---
+++ Shared libraries/libMUSE/serializacionMUSE.c
@@ -0,0 +1,32 @@
+//Bibliotecas propias
+#include "libMUSE.h"
+
+void* crear_paquete_init(int codigo_de_operacion, int id) {
+	//Todos los pesos de los elementos
+	int peso_total=0;
+	int peso_codigo_de_operacion=sizeof(int);
+	int peso_del_siguiente;
+
+	//Suma del peso total: sizeof(tamanio del elem)+sizeof(elem) --> Sumado por cada elem.
+	//El peso del codigo_de_operacion no tiene sentido ponerlo, ya se que es un int.
+	peso_total+=peso_codigo_de_operacion;
+
+	peso_del_siguiente=sizeof(id);
+	peso_total+=sizeof(peso_del_siguiente);
+	peso_total+=peso_del_siguiente;
+
+	void* paquete=(void*)malloc(peso_total);
+
+	int offset=0;
+	peso_del_siguiente=0;
+
+	memcpy(paquete+offset, &codigo_de_operacion, peso_codigo_de_operacion);
+	offset+=peso_codigo_de_operacion;
+
+	peso_del_siguiente=sizeof(id);
+	memcpy(paquete+offset, &peso_del_siguiente, sizeof(peso_del_siguiente));
+	offset+=sizeof(peso_del_siguiente);
+	memcpy(paquete+offset, &id, peso_del_siguiente);
+
+	return paquete;
+}
---
+++ Shared libraries/libMUSE/serializacionMUSE.h
@@ -0,0 +1,6 @@
+#ifndef SERIALIZACIONMUSE_H_
+#define SERIALIZACIONMUSE_H_
+
+void* crear_paquete_init(int codigo_de_operacion, int id);
+
+#endif /* SERIALIZACIONMUSE_H_ */
