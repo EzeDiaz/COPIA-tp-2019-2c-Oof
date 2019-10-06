6faac7d8e63f04605c4ba83062903e500a5e2a74
juancueli99 <juanfcue99@gmail.com>
2019-10-06 20:17:30 -0300

    Agregamos funcionalidades del servidor

---
+++ SUSE/SAC/funcionalidades_SAC.c
@@ -0,0 +1,45 @@
+/*
+ * funcionalidades_SAC.c
+ *
+ *  Created on: 6 oct. 2019
+ *      Author: utnso
+ */
+
+#include "funcionalidades_SAC.h"
+
+//Funcionalidades con archivos
+bool crear_archivo(){
+	bool flag = false;
+	return flag;
+}
+
+bool escribir_archivo(){
+	bool flag = false;
+	return flag;
+}
+
+t_list* leer_archivo(){
+	t_list* lista_de_retorno = list_create();
+	return lista_de_retorno;
+}
+
+bool borrar_archivo(){
+	bool flag = false;
+	return flag;
+}
+
+//Funcionalidades con directorios
+bool crear_directorio(){
+	bool flag = false;
+	return flag;
+}
+
+t_list* listar_directorio_y_archivos(){
+	t_list* lista_de_retorno = list_create();
+	return lista_de_retorno;
+}
+
+bool eliminar_directorio(){
+	bool flag = false;
+	return flag;
+}
---
+++ SUSE/SAC/funcionalidades_SAC.h
@@ -0,0 +1,28 @@
+/*
+ * funcionalidades_SAC.h
+ *
+ *  Created on: 6 oct. 2019
+ *      Author: utnso
+ */
+
+#ifndef FUNCIONALIDADES_SAC_H_
+#define FUNCIONALIDADES_SAC_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <commons/collections/list.h>
+
+//Funcionalidades con archivos
+bool crear_archivo();
+bool escribir_archivo();
+t_list* leer_archivo();
+bool borrar_archivo();
+
+//Funcionalidades con directorios
+bool crear_directorio();
+t_list* listar_directorio_y_archivos();
+bool eliminar_directorio();
+
+
+#endif /* FUNCIONALIDADES_SAC_H_ */
