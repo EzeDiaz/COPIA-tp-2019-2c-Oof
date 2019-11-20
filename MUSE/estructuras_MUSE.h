4feb9aa61a1c077568d61a0ddfd572891cd5d1ec
EzeDiaz <ezequiel.dz@gmail.com>
2019-11-20 15:32:16 -0300

    Fix conflicting types

--- MUSE/estructuras_MUSE.h
+++ MUSE/estructuras_MUSE.h
@@ -98,7 +98,7 @@ void SUBSTRACT_MEMORY_LEFT(int size);
 segment* GET_SEGMENT__ADDRESS(uint32_t, addressSpace*);
 int GET_FRAME_FROM_ADDRESS(uint32_t address, segment* a_segment);
 void MERGE_CONSECUTIVES_FREE_BLOCKS(segment* a_segment);
-void FREE_USED_FRAME(uint32_t address, addressSpace* address_space);
+int FREE_USED_FRAME(uint32_t address, addressSpace* address_space);
 heapMetadata* GET_METADATA_BEHIND_ADDRESS(uint32_t address, t_list* page_frame_table);
 
 //Potentially deprecated
